7641ee45ce24f60a08b80dadd65277f13df4de25
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2023-12-28 12:34:40 +0800

    Fix Career Map

--- src/components/CareerMap.vue
+++ src/components/CareerMap.vue
@@ -1,21 +1,42 @@
 <template>
-    <img src="/Career Map.jpg" alt="DICT Logo" class="logo h-100 p-0" />
-    <div class="absolute-container">
-        <div class="absolute-text" style="left: 25px; top: 1px; text-align: center;" title="This career path focuses on using data to inform business decisions and strategy. Professionals in this field analyze data trends to provide insights that drive business growth, efficiency, and innovation. They often work with management teams to understand business objectives and translate data findings into actionable strategies.">Business<br> Intelligence &<br> Strategy</div>
-        <div class="absolute-text" style="left: 165px; top: 30px;" title="This career path focuses on using data to inform business decisions and strategy. Professionals in this field analyze data trends to provide insights that drive business growth, efficiency, and innovation. They often work with management teams to understand business objectives and translate data findings into actionable strategies.">Data Stewardship</div>
-        <div class="absolute-text" style="left: 325px; top: 30px;" title="Data engineers design, build, and maintain the systems and infrastructure that collect, process, and store data. They are responsible for the technical architecture of data projects, including the integration of disparate data sources, data transformation, and the creation of databases and data warehouses.">Data Engineering</div>
-        <div class="absolute-text" style="left: 495px; top: 30px;" title="Data scientists analyze and interpret complex data to help organizations make better decisions. They use statistical techniques, machine learning, and predictive modeling to extract insights from data. Their work involves not only analyzing data but also developing new algorithms and models to solve specific business problems.">Data Science</div>
-        <div class="absolute-text" style="left: 645px; top: 30px;" title="AI engineers focus on developing, deploying, and maintaining AI systems. This involves programming, algorithm development, and working with machine learning and deep learning models. They collaborate closely with data scientists to implement AI solutions and ensure that they are scalable, efficient, and effective.">AI Engineering</div>
-        <div class="absolute-text" style="left: 795px; top: 15px; text-align: center;" title="This career track is for professionals who conduct research in data analytics and AI. They work on developing new methods, algorithms, and technologies in the field. Their focus is often on pushing the boundaries of what is currently possible in analytics and AI, contributing to academic knowledge as well as practical applications.">Applied Data/AI<br>Research</div>
-        <div class="absolute-text" style="left: 965px; top: 15px; text-align: center;" title="This career path in data and AI education for aspiring teachers, trainers, and learning providers encompasses a journey from introductory instructional roles to advanced educational leadership, specializing in imparting and developing cutting-edge analytics and AI knowledge and skills.">Data/AI<br>Education</div>
-    </div>
+  <img src="/Career Map.jpg" alt="DICT Logo" class="logo h-100 p-0" />
+  <div class="file-upload mt-2 mb-4 d-flex justify-content-end  ">
+    <input type="file" @change="handleFileUpload" accept=".xlsx, .xls" />
+    <!-- You can customize the 'accept' attribute based on the file types you want to allow -->
+  </div>
+  <div class="absolute-container">
+    <div class="absolute-text" style="left: 25px; top: 1px; text-align: center;"
+      title="This career path focuses on using data to inform business decisions and strategy. Professionals in this field analyze data trends to provide insights that drive business growth, efficiency, and innovation. They often work with management teams to understand business objectives and translate data findings into actionable strategies.">
+      Business<br> Intelligence &<br> Strategy</div>
+    <div class="absolute-text" style="left: 165px; top: 30px;"
+      title="This career path focuses on using data to inform business decisions and strategy. Professionals in this field analyze data trends to provide insights that drive business growth, efficiency, and innovation. They often work with management teams to understand business objectives and translate data findings into actionable strategies.">
+      Data Stewardship</div>
+    <div class="absolute-text" style="left: 325px; top: 30px;"
+      title="Data engineers design, build, and maintain the systems and infrastructure that collect, process, and store data. They are responsible for the technical architecture of data projects, including the integration of disparate data sources, data transformation, and the creation of databases and data warehouses.">
+      Data Engineering</div>
+    <div class="absolute-text" style="left: 495px; top: 30px;"
+      title="Data scientists analyze and interpret complex data to help organizations make better decisions. They use statistical techniques, machine learning, and predictive modeling to extract insights from data. Their work involves not only analyzing data but also developing new algorithms and models to solve specific business problems.">
+      Data Science</div>
+    <div class="absolute-text" style="left: 645px; top: 30px;"
+      title="AI engineers focus on developing, deploying, and maintaining AI systems. This involves programming, algorithm development, and working with machine learning and deep learning models. They collaborate closely with data scientists to implement AI solutions and ensure that they are scalable, efficient, and effective.">
+      AI Engineering</div>
+    <div class="absolute-text" style="left: 795px; top: 15px; text-align: center;"
+      title="This career track is for professionals who conduct research in data analytics and AI. They work on developing new methods, algorithms, and technologies in the field. Their focus is often on pushing the boundaries of what is currently possible in analytics and AI, contributing to academic knowledge as well as practical applications.">
+      Applied Data/AI<br>Research</div>
+    <div class="absolute-text" style="left: 965px; top: 15px; text-align: center;"
+      title="This career path in data and AI education for aspiring teachers, trainers, and learning providers encompasses a journey from introductory instructional roles to advanced educational leadership, specializing in imparting and developing cutting-edge analytics and AI knowledge and skills.">
+      Data/AI<br>Education</div>
+  </div>
+  
 </template>
 
 <style>
 .absolute-container {
   position: absolute;
-  left: 300px; /* Set the x position */
-  top: 130px; /* Set the y position */
+  left: 300px;
+  /* Set the x position */
+  top: 130px;
+  /* Set the y position */
   width: 1100px;
   height: 80px;
   border: 0px solid #ccc;
@@ -31,10 +52,14 @@
   font-weight: bold;
 }
 
+.file-upload {
+    text-align: center;
+    color: white;
+}
+
 </style>
 
 <script setup>
-
 
 
 </script>
