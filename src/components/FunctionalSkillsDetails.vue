8227807c35459ab936708711bce88c95bcfbb922
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2023-12-31 00:18:25 +0800

    Update FunctionalSkillsDetails.vue

--- src/components/FunctionalSkillsDetails.vue
+++ src/components/FunctionalSkillsDetails.vue
@@ -2,67 +2,127 @@
     <div>
       <table class="styled-table mb-3">
         <tr>
-          <td style="border: 1px solid white;">Enabling Skill</td>
-          <th colspan="6" style="border: 1px solid white; text-align: center;">ESC-DGF: Digital Fluency</th>
+          <td style="border: 1px solid white;">Functional Skill</td>
+          <th colspan="11" style="border: 1px solid white; text-align: start;">AAI-DEN: Data Engineering</th>
         </tr>
         <tr>
           <td style="border: 1px solid white;">Category</td>
-          <th colspan="6" style="border: 1px solid white; text-align: center;">Staying Relevant</th>
+          <th colspan="11" style="border: 1px solid white; text-align: start;">Development and Implementation</th>
 
         </tr>
         <tr>
           <td style="border: 1px solid white;">Related Category</td>
-          <th colspan="6" style="border: 1px solid white; text-align: center;">21st Century Skills</th>
+          <th colspan="11" style="border: 1px solid white; text-align: start;">Data Engineering</th>
         </tr>
         <tr>
           <td style="border: 1px solid white;">Description</td>
-          <th colspan="6" style="border: 1px solid white; text-align: center;">Leverage digital technology tools, systems, and software across work processes and activities to solve problems, drive efficiency and facilitate information sharing</th>
+          <th colspan="11" style="border: 1px solid white; text-align: start;">Develop and implement efficient and stable processes to collect, store, extract, transform, load and integrate data at various stages in the data pipeline. This also involves processing varying amounts of data from a variety of sources and preparing data in a structure that is easily access and analysed according to business requirements</th>
         </tr>
         <tr>
           <th rowspan="3" style="border: 1px solid white; text-align: center;">Proficiency Description</th>
-          <th colspan="2" style="border: 1px solid white; text-align: center;">Basic</th>
-          <th colspan="2" style="border: 1px solid white; text-align: center;">Intermediate</th>
-          <th colspan="2" style="border: 1px solid white; text-align: center;">Advance</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">1</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">2</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">3</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">4</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">5</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">6</th>
         </tr>
         <tr>
-          <th colspan="2" style="border: 1px solid white; text-align: center;">ESC-DGF-B</th>
-          <th colspan="2" style="border: 1px solid white; text-align: center;">ESC-DGF-I</th>
-          <th colspan="2" style="border: 1px solid white; text-align: center;">Advance</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">AAI-CTR-1</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">AAI-CTR-2</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">AAI-CTR-3</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;">AAI-CTR-4</th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;"></th>
+          <th colspan="2" style="border: 1px solid white; text-align: center;"></th>
         </tr>
         <tr>
-          <td style="border: 1px solid white;">Row 7, Col 2</td>
-          <td style="border: 1px solid white;">Row 7, Col 3</td>
-          <td style="border: 1px solid white;">Row 7, Col 4</td>
-          <td style="border: 1px solid white;">Row 7, Col 5</td>
-          <td style="border: 1px solid white;">Row 7, Col 6</td>
-          <td style="border: 1px solid white;">Row 7, Col 7</td>
+          <th colspan="2" style="border: 1px solid white; text-align: start;">Label, track and document all configuration items and changes to software projects using standard tools and templates</th>
+          <th colspan="2" style="border: 1px solid white; text-align: start;">Verify accuracy, completeness and currency of information in configuration logs and review unauthorised changes, diversions or inappropriate use of software assets</th>
+          <th colspan="2" style="border: 1px solid white; text-align: start;">Develop and update a configuration management plan, determining systems and techniques to track changes and revisions</th>
+          <th colspan="2" style="border: 1px solid white; text-align: start;">Develop policies, processes and guidelines for the organisation's  configuration management and tracking</th>
+          <th colspan="2" style="border: 1px solid white; text-align: start;"></th>
+          <th colspan="2" style="border: 1px solid white; text-align: start;"></th>
         </tr>
         <tr>
-          <td style="border: 1px solid white;">Row 8, Col 1</td>
-          <td style="border: 1px solid white;">Row 8, Col 2</td>
-          <td style="border: 1px solid white;">Row 8, Col 3</td>
-          <td style="border: 1px solid white;">Row 8, Col 4</td>
-          <td style="border: 1px solid white;">Row 8, Col 5</td>
-          <td style="border: 1px solid white;">Row 8, Col 6</td>
-          <td style="border: 1px solid white;">Row 8, Col 7</td>
+          <td style="border: 1px solid white;">Knowledge</td>
+          <th colspan="2" style="border: 1px solid white; text-align: start; vertical-align: top;">
+            <ul>
+                <li>Key information required to label configuration items</li>
+                <li>Indicators of unauthorised changes, diversions or inappropriate use</li>
+                <li>Key information required to label configuration items</li>
+                <li>Indicators of unauthorised changes, diversions or inappropriate use</li>
+                <li>Configuration tracking processes and tools</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: start; vertical-align: top">
+            <ul>
+                <li>Importance of baselines in configuration items</li>
+                <li>Process of handling unauthorised changes, diversions or inappropriate use</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;">
+            <ul>
+                <li>Key components of a configuration management plan</li>
+                <li>Various configuration management / tracking systems, tools and techniques</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;">
+            <ul>
+                <li>Best practices in management of configuration items</li>
+                <li>Industry requirements and standards in the protection of software assets</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;"></th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;"></th>
         </tr>
         <tr>
-          <td style="border: 1px solid white;">Row 9, Col 1</td>
-          <td style="border: 1px solid white;">Row 9, Col 2</td>
-          <td style="border: 1px solid white;">Row 9, Col 3</td>
-          <td style="border: 1px solid white;">Row 9, Col 4</td>
-          <td style="border: 1px solid white;">Row 9, Col 5</td>
-          <td style="border: 1px solid white;">Row 9, Col 6</td>
-          <td style="border: 1px solid white;">Row 9, Col 7</td>
+            <td style="border: 1px solid white;">Knowledge</td>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;">
+            <ul>
+                <li>Label all configuration items according to set instructions and templates</li>
+                <li>Track configuration items, components and changes</li>
+                <li>Document changes to software projects using standard tools and templates</li>
+                <li>Perform regular checks on configuration and report unauthorised changes, diversions or inappropriate use of software products</li>
+                <li>Label all configuration items according to set instructions and templates</li>
+                <li>Track configuration items, components and changes</li>
+                <li>Document changes to software projects using standard tools and templates</li>
+                <li>Perform regular checks on configuration and report unauthorised changes, diversions or inappropriate use of software products</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;">
+            <ul>
+                <li>Review key information on configuration items</li>
+                <li>Track baselines for configuration items</li>
+                <li>Generate reports on configuration status for tracking software project progress</li>
+                <li>Verify for accuracy, and completeness of information in configuration logs and records</li>
+                <li>Review software assets where unauthorised changes, diversions or inappropriate use has occurred</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;">
+            <ul>
+                <li>Develop a configuration management plan to oversee the systematic tracking, control and management of changes in software projects</li>
+                <li>Develop identification standards for naming and version control of software documentation</li>
+                <li>Select appropriate systems, tools and techniques to track changes and revisions</li>
+                <li>Update configuration management plan to account for key enhancements or updates to software assets</li>
+                <li>Investigate and report areas of non-compliance with configuration management standards or unaccounted changes</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top;">
+            <ul>
+                <li>Develop guidelines for the consistent classification and management of configuration items</li>
+                <li>Develop policies for the retention of baseline copies</li>
+                <li>Facilitate to oversee the organisation's configuration management and tracking system</li>
+                <li>Establish processes for the verification and audit of configuration records</li>
+                <li>Establish compliance standards to account for all changes to software products</li>
+            </ul>
+          </th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top; "></th>
+          <th colspan="2" style="border: 1px solid white; text-align: center; vertical-align: top; "></th>
         </tr>
         <tr>
-          <td style="border: 1px solid white;">Row 10, Col 1</td>
-          <td style="border: 1px solid white;">Row 10, Col 2</td>
-          <td style="border: 1px solid white;">Row 10, Col 3</td>
-          <td style="border: 1px solid white;">Row 10, Col 4</td>
-          <td style="border: 1px solid white;">Row 10, Col 5</td>
-          <td style="border: 1px solid white;">Row 10, Col 6</td>
-          <td style="border: 1px solid white;">Row 10, Col 7</td>
+          <td style="border: 1px solid white;">Range of Application</td>
+          <th colspan="11" style="border: 1px solid white; text-align: start;">Test Data</th>
+
         </tr>
       </table>
     </div>
@@ -82,6 +142,8 @@
   /* Add your additional styles here */
   .styled-table {
     border-collapse: collapse;
+    margin-left: 50px;
+    margin-right: 50px;
   }
   
   .styled-table th, .styled-table td {
