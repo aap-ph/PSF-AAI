a156abec592479b5fc16fc0c1923ab9c561e7514
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2023-12-30 11:52:37 +0800

    404 not found fix

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import RevisionLog from '../views/RevisionLogView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  mode:'history',
   routes: [
     {
       path: '/',
