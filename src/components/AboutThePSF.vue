c4e680419fa41ffc380dfbedcb7ea43852bb91aa
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2023-12-30 11:46:31 +0800

    About PSF and Acknowledgement

--- src/components/AboutThePSF.vue
+++ src/components/AboutThePSF.vue
@@ -1,4 +1,37 @@
 <template>
-    This is the About the PSF Page
-</template>
+    <div class="text-light text pt-3">
+      <p>
+        The Philippine Skills Framework (PSF) Initiative is a Philippine government inter-agency effort to build the skills and competencies of the country’s human capital and to better prepare the workforce for the future economy, particularly for Industry 4.0. The PSF Initiative’s Memorandum of Understanding (MOU) were signed by ten departments and agencies, namely the Department of Trade and Industry (DTI), Technical Education and Skills Development Authority (TESDA), Department of Labor and Employment (DOLE), Department of Education (DepEd), Commission on Higher Education (CHED), Professional Regulation Commission (PRC), Department of Information and Communications Technology (DICT), Department of Science and Technology (DOST), Department of Tourism (DOT) and the Department of Agriculture (DA). The PSF Initiative prioritizes the construction, creatives, food (agriculture and fishery), health and wellness, IT-BPM (Information Technology and Business Process Management), logistics and supply chain, manufacturing, and tourism industries. 
+      </p>
+  
+      <p>
+        The Philippine Skills Framework for Analytics and Artificial Intelligence (PSF-AAI) is an endeavor developed for the Philippine workforce of all industries to address skills mastery and continuous learning in the AAI Sector. It is a collaborative development effort initiated and spearheaded by the Department of Information and Communication Technology (DICT), the Analytics and Artificial Intelligence Association of the Philippines (AAP), and Thames International Business School, in cooperation with the stakeholders of the Philippine AAI field, i.e., employers, practitioners, education and training providers.
+      </p>
+  
+      <p>
+        The seven tracks of the PSF-AAI consist of (1) Business Intelligence & Strategy, (2) Data Stewardship, (3) Data Engineering, (4) Data Science, (5) AI Engineering, (6) Applied Data/AI Research, and (7) Analytics/AI Education.
+      </p>
+  
+      <p>
+        These verticals were chosen by the Philippine AAI Experts with Philippine Subject Matter Experts and Consultants as part of the evolution of AAP’s Professional Maturity Model (which was derived from the 2017 APEC Project DARE or Data Analytics Raising Employment). This PSF is also informed by the results of the 2022 Labor Market Intelligence Report on the Analytics Sector as part of the A Future That Works project from the Philippine Business for Education (PBEd) and Australia’s Department of Foreign Affairs and Trade (AU-DFAT). This PSF is a result of a thorough conduct of studies, desk research, and review. The PSF methodology consisted of the referencing of the Singapore Skills Framework, and formation of the various teams. Then, the contextualization by Filipino subject matter experts—the Contextualizers—was done to adapt it to the Philippine industry scenarios. After, the Validators—AAI subject matter experts from the various stakeholders of the Philippine industry, i.e., practitioners, the private sector, government, and academe—worked together to ascertain the validity of the draft PSF-AAI framework to local best practices.
+      </p>
+  
+      <p>
+        The Philippine Skills Framework for Analytics and Artificial Intelligence provides useful information on sector and employment opportunities, career pathways, occupation and job roles, functional skills and competencies, enabling skills and competencies.
+      </p>
+    </div>
+  </template>
+
+
+<style>
+.text{
+    padding-left: 100px !important;
+    padding-right: 100px !important;   
+    padding-bottom: 50px !important;   
+    text-align: justify !important;
+}
+
+
+
+</style>
   
