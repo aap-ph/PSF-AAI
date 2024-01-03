1c26230dd06b87067ea262c2618ebe7525c34a47
Chris John Pajal <115773035+JohnasaurusRex@users.noreply.github.com>
2024-01-03 14:32:18 +0800

    Update SkillMapMain.vue

--- src/components/SkillMapMain.vue
+++ src/components/SkillMapMain.vue
@@ -1,30 +1,536 @@
 <template>
-    <table class="styled-table mb-3">
-        <tr>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Skill Type</th>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Skill Category</th>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Related Category</th>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Skills</th>
-        </tr>
-        <tc>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Skill Type</th>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Skill Category</th>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Related Category</th>
-            <th rowspan="2" style="border: 1px solid white; text-align: center;">Skills</th>
-
-        </tc>
-        
-    
-
-
-
-
-    </table>
-</template>
-
+    <div class="container">
+      <div class="fixed-width" ref="fixedWidth">
+        <!-- Left Half Content Goes Here -->
+        <table id="left-table">
+            <thead>
+                <tr style="height: 120.8px;">
+                    <th>Skill Type</th>
+                    <th>Skill Category</th>
+                    <th>Related Category</th>
+                    <th>Skill</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Interacting with Others</td>
+                    <td>21st Century Skills</td>
+                    <td>Building Inclusivity</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Interacting with Others</td>
+                    <td>21st Century Skills</td>
+                    <td>Collaboration</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Interacting with Others</td>
+                    <td>21st Century Skills</td>
+                    <td>Communication</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Interacting with Others</td>
+                    <td>21st Century Skills</td>
+                    <td>Customer Orientation</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Interacting with Others</td>
+                    <td>21st Century Skills</td>
+                    <td>Developing People</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Interacting with Others</td>
+                    <td>21st Century Skills</td>
+                    <td>Influence</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Staying Relevant</td>
+                    <td>21st Century Skills</td>
+                    <td>Adaptability</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Staying Relevant</td>
+                    <td>21st Century Skills</td>
+                    <td>Digital Fluency</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Staying Relevant</td>
+                    <td>21st Century Skills</td>
+                    <td>Global Perspective</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Staying Relevant</td>
+                    <td>21st Century Skills</td>
+                    <td>Learning Agility</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Staying Relevant</td>
+                    <td>21st Century Skills</td>
+                    <td>Self Management</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Thinking Critically</td>
+                    <td>21st Century Skills</td>
+                    <td>Creative Thinking</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Thinking Critically</td>
+                    <td>21st Century Skills</td>
+                    <td>Decision Making</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Thinking Critically</td>
+                    <td>21st Century Skills</td>
+                    <td>Problem Solving</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Thinking Critically</td>
+                    <td>21st Century Skills</td>
+                    <td>Sense Making</td>
+                </tr>
+                <tr>
+                    <td>Enabling</td>
+                    <td>Thinking Critically</td>
+                    <td>21st Century Skills</td>
+                    <td>Transdisciplinary Thinking</td>
+                </tr>
+                <tr>
+                    <td>Functional</td>
+                    <td>Analytical Thinking</td>
+                    <td></td>
+                    <td>Systems Thinking</td>
+                </tr>
+                <tr>
+                    <td>Functional</td>
+                    <td>Business and Project Management</td>
+                    <td></td>
+                    <td>Business Agility</td>
+                </tr>
+                <tr>
+                    <td>Functional</td>
+                    <td>Business and Project Management</td>
+                    <td></td>
+                    <td>Business Continuity</td>
+                </tr>
+            </tbody>
+        </table>
+      </div>
+      <div class="scrollable" ref="scrollable">
+        <!-- Right Half Content Goes Here -->
+        <table id="right-table" >
+          <thead>
+            <th colspan="7" style="font-size: 14px; padding: 0px;">Business Intelligence & Strategy</th>
+            <th colspan="7" style="font-size: 14px; padding: 0px;">Data Steward</th>      
+            <tr>
+              <th>Associate Data Analyst</th>
+              <th>Data Analyst</th>
+              <th>Business Intelligence Analyst</th>
+              <th>Senior Business Intelligence Analyst</th>
+              <th>Business Analytics Manager</th>
+              <th>Business Analytics Director</th>
+              <th>Chief [Business Function] Officer</th>
+              <th>Data Protection Specialist</th>
+              <th>Data Governance Manager</th>
+              <th>Data Governance Officer</th>
+              <th>Chief Data Officer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+            </tr>
+            <tr>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>3</td>
+                <td>4</td>
+                <td>4</td>
+                <td>5</td>
+                <td>3</td>
+                <td>4</td>
+                <td>4</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td></td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td></td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+            </tr>
+            <tr>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <tr>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Basic</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+                <td>Intermediate</td>
+                <td>Intermediate</td>
+                <td>Advanced</td>
+                <td>Advanced</td>
+            </tr>
+            <!-- Add more rows as needed -->
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  export default {
+    mounted() {
+      this.syncScroll();
+    },
+    methods: {
+      syncScroll() {
+        const fixedWidth = this.$refs.fixedWidth;
+        const scrollable = this.$refs.scrollable;
+  
+        // Set the width and height of fixed-width to match scrollable
+        // fixedWidth.style.width = `${scrollable.clientWidth}px`;
+        fixedWidth.style.height = `${scrollable.clientHeight}px`;
+  
+        // Add event listener to synchronize scroll from scrollable to fixed-width
+        scrollable.addEventListener("scroll", () => {
+          fixedWidth.scrollTop = scrollable.scrollTop;
+        });
+  
+        // Add event listener to synchronize scroll from fixed-width to scrollable
+        fixedWidth.addEventListener("scroll", () => {
+          scrollable.scrollTop = fixedWidth.scrollTop;
+        });
+      },
+    },
+  };
+  </script>
+  
 <style scoped>
-.styled-table{
-    color: white;
+.container {
+  display: flex;
+  padding: 0px;
+  max-height: 740px !important;
 }
 
+#left-table {
+    white-space: nowrap;
+
+}
+
+.fixed-width::-webkit-scrollbar {
+    display: none;
+}
+
+.fixed-width {
+  width: 200%; /* Adjust the fixed width as needed */
+  box-sizing: border-box;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+
+
+
+.scrollable {
+  overflow-x: auto;
+  overflow-y: scroll;
+  min-width: 0; /* Add this line to allow the div to shrink below its content size */
+}
+
+.scrollable th {
+  text-align: center;
+}
+
+thead {
+  position: sticky;
+  top: 0;
+  background-color: #44546a;
+  color: white;
+  z-index: 1;
+}
+
+thead tr, th {
+    border: 1px solid #ddd;
+    border-top: 1px solid #ddd;
+}
+
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+  color: white;
+}
+
+
 </style>
+  
