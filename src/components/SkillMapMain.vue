1e99bfb5a8ffa6b5d12f81d47bfa2cbe22995f9c
Chris John Pajal <115773035+JohnasaurusRex@users.noreply.github.com>
2024-01-04 22:41:55 +0800

    Update SkillMapMain.vue

--- src/components/SkillMapMain.vue
+++ src/components/SkillMapMain.vue
@@ -4,7 +4,7 @@
             <!-- Left Half Content Goes Here -->
             <table id="left-table">
                 <thead>
-                    <tr style="height: 123px; text-align: center;">
+                    <tr class="first-header">
                         <th>Skill Type</th>
                         <th>Skill Category</th>
                     </tr>
@@ -390,24 +390,34 @@ function syncScroll() {
     display: none;
 }
 
+/* .scrollable::-webkit-scrollbar:vertical{
+    height: 10px;
+} */
+
 .fixed-width {
     width: 200%; /* Adjust the width as needed */
     box-sizing: border-box;
     overflow-y: scroll;
     -ms-overflow-style: none;
     scrollbar-width: none;
+    height: 800px;
 }
 
-#right-table {
-    width: 300%; /* Adjust the width as needed */
-    box-sizing: border-box;
+.first-header {
+    height: 123px;
+    text-align: center;
 }
+/* #right-table {
+    width: 50%; 
+    box-sizing: border-box;
+} */
 
 .scrollable {
     overflow-x: auto;
     overflow-y: scroll;
     min-width: 0;
     height: 815px;
+    width: 300%;
     /* Add this line to allow the div to shrink below its content size */
 }
 
@@ -441,4 +451,33 @@ td {
     text-align: left;
     color: white;
 }
+
+@media (max-width: 1600px) {
+    .fixed-width {
+        height: 600px !important;
+    }
+    .first-header {
+        height: 122px;
+    }
+    .scrollable {
+        height: 612px !important;
+        width: 300% !important;
+    }
+}
+
+@media (max-width: 1377px) {
+    .fixed-width {
+        height: 500px !important;
+    }
+    .first-header {
+        height: 122px;
+    }
+    .scrollable {
+        height: 515px !important;
+        width: 200% !important;
+    }
+}
+
+
 </style>
+
