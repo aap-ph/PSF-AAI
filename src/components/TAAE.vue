800667ded833bd0fd88b806865ae962a03f8f723
Analytics & Artificial Intelligence Association of the Philippines <sherwin.pelayo@aap.ph>
2024-01-11 09:26:02 +0800

    Test TAAE.vue

--- src/components/TAAE.vue
+++ src/components/TAAE.vue
@@ -1,4 +1,10 @@
 <template>
-    This is the Analytics & AI Ecosystem Page
+    <p> This is the Analytics & AI Ecosystem Page </p>
 </template>
-  
+<style>
+ p {
+    font-weight: bold;
+    font-style: italic;
+    color: white;
+ }
+</style>  
