1e8f96d066c6f6531fe5b003aeac3b7cf777b52c
Analytics & Artificial Intelligence Association of the Philippines <sherwin.pelayo@aap.ph>
2024-01-20 16:00:50 +0800

    Update Contact Us

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,4 +1,10 @@
 <template>
-    This is the Contact Us Page
+    <p>We would appreciate any feedback about the Philippine Skills Framework for Analytics & AI.</p>
+    <p>Please fill-up this form for your comments: <a href="https://bit.ly/psf-aai_validation_form">[Feedback]</a>.</p>
 </template>
-  
+<style scoped>
+ p {
+    
+    color: white;
+ }
+</style>   
