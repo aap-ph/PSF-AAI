f8e45764ae3a811029c8294bde53a5e545327b7e
Analytics & Artificial Intelligence Association of the Philippines <sherwin.pelayo@aap.ph>
2024-01-23 15:21:18 +0800

    Update Contact Us

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,10 +1,35 @@
 <template>
+    <div class="text-light text pt-3">
     <p>We would appreciate any feedback about the Philippine Skills Framework for Analytics & AI.</p>
     <p>Please fill-up this form for your comments: <a href="https://bit.ly/psf-aai_validation_form">[Feedback]</a>.</p>
+    <p></p>
+    <p>The AAP will also be hosting several 2-hour Zoom sessions to get your feedback live. In these sessions, the AAP will be providing a short overview of the PSF-AAI and then will proceed to open the floor for feedback. If you wish to attend any of these sessions, please register through the following links:</p>
+    <ul>
+    <li>Thursday, Jan 25, 10am: <a href="https://bit.ly/psf-aai_validation_jan25am">bit.ly/psf-aai_validation_jan25am</a></li>
+    <li>Thursday, Jan 25, 2pm: <a href="https://bit.ly/psf-aai_validation_jan25pm">bit.ly/psf-aai_validation_jan25pm</a></li>
+    <li>Saturday, Feb 3, 10am: <a href="https://bit.ly/psf-aai_validation_feb3am">bit.ly/psf-aai_validation_feb3am</a></li>
+    <li>Monday, Feb 12, 10am: <a href="https://bit.ly/psf-aai_validation_feb12am">bit.ly/psf-aai_validation_feb12am</a></li>
+    <li>Monday, Feb 12, 2pm: <a href="https://bit.ly/psf-aai_validation_feb12pm">bit.ly/psf-aai_validation_feb12pm</a></li>
+</ul>
+<p>Zoom links, including calendar placeholders, will be emailed to you once you have registered. You may attend multiple sessions as you wish. Prior to attending a session, please review already the PSF-AAI microsite and its contents and be ready with your feedback.
+</p>
+<p>Your feedback will be important to achieve the goal of this PSF-AAI initiative: to prepare our current and future workforce in analytics and AI by providing the right education and training that are aligned with industry needs.
+</p>
+<p>We will be accepting feedback for the first official version of this PSF-AAI until Friday, February 9, 2024 . Feedback received after this date will be incorporated in the succeeding versions of the PSF-AAI.</p>
+<p>Should you have questions and/or clarifications about the PSF-AAI, please email <a href="mailto:psf.aai@aap.ph">psf.aai@aap.ph.</a>
+</p>
+<p>Looking forward to your participation in this nationwide initiative of preparing our current and future workforce in analytics and AI.</p>
+</div>
 </template>
 <style scoped>
  p {
     
     color: white;
  }
+ .text{
+    padding-left: 100px !important;
+    padding-right: 100px !important;   
+    padding-bottom: 50px !important;   
+    text-align: justify !important;
+}
 </style>   
